1d1e951820b2e7451054fd7f35ba5e80d7427a74
Kaustubh-77 <129765047+Kaustubh-77@users.noreply.github.com>
2026-02-22 12:51:34 +0530

    Update solution for precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -3,13 +3,12 @@ def precision_recall_at_k(recommended, relevant, k):
     Compute precision@k and recall@k for a recommendation list.
     """
     # Write code here
-    # prec=len(set(recommended).intersection(set(relevant)))/k
-    # rec=len(set(recommended).intersection(set(relevant)))/len(relevant)
-    # return [prec,rec]
-    recommended_k = recommended[:k]
-    hits = len(set(recommended_k) & set(relevant))
+    recommended=recommended[:k]
+    prec=len(set(recommended).intersection(set(relevant)))/k
+    if k==0:
+        prec=0
+    rec=len(set(recommended).intersection(set(relevant)))/len(relevant)
+    if len(relevant)==0:
+        rec=0
+    return [prec,rec]
     
-    precision = hits / k if k > 0 else 0
-    recall = hits / len(relevant) if len(relevant) > 0 else 0
-    
-    return [precision, recall]
